08e788c6c6d09cc14b709012ae346b317b50848e
Mahlangu <34237978+PTMahlangu@users.noreply.github.com>
2021-09-29 08:42:08 +0200

    Update products.js

--- Components/products.js
+++ Components/products.js
@@ -1,15 +1,179 @@
 const products  = [
-    {
-    id = 1,
-    product = "banana",
-    quantity= 300,
-    category = "fruit"
-    // picture_url = "https://what what"
-},
-{
-    // product2
-},
-{
 
-}
+{
+  id = 1,
+  product = "Onions",
+  quantity= 300,
+  category = "Vegetable",
+  price:"$ 0.9/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 2,
+  product = "Potatoes",
+  quantity= 300,
+  category = "Vegetable",
+  price:"$ 1.9/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 3,
+  product = "Beans",
+  quantity= 300,
+  category = "Vegetable",
+  price:"$ 2.9/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 4,
+  product = "Cabbage",
+  quantity= 300,
+  category = "Vegetable",
+  price:"$ 1.2/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 5,
+  product = "Pumkins",
+  quantity= 300,
+  category = "Vegetable",
+  price:"$ 1.6/kg",
+  picture_url = "https://what what"
+},
+
+
+{
+  id = 6,
+  product = "Watermelon",
+  quantity= 300,
+  category = "Fruit",
+  price:"$ 1.8/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 7,
+  product = "Pineapples",
+  quantity= 300,
+  category = "Fruit",
+  price:"$ 2.2/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 8,
+  product = "Grapes",
+  quantity= 300,
+  category = "Fruit",
+  price:"$ 3.2/kg",
+  picture_url = "https://what what"
+},
+{
+  id = 9,
+  product = "Straberries",
+  quantity= 300,
+  category = "Fruit",
+  price:"$ 2.1/kg",
+  picture_url = "https://what what"
+},
+{
+  id =10,
+  product = "Avocadoes",
+  quantity= 300,
+  category = "Fruit",
+  price:"$ 3.2/kg",
+  picture_url = "https://what what"
+},
+
+{
+  id = 11,
+  product = "Eggs",
+  quantity= 300,
+  category = "Dairy",
+  price:"$ 14.00/Cartons",
+  picture_url = "https://what what"
+},
+{
+  id =12,
+  product = "Chikenes",
+  quantity= 300,
+  category = "Poultry",
+  price:"$ 10.0/box",
+  picture_url = "https://what what"
+},
+{
+  id =12,
+  product = "Cheese",
+  quantity= 300,
+  category = "Dairy",
+  price:"$ 2.0/box",
+  picture_url = "https://what what"
+},
+
+
+{
+  id = 14,
+  product = "Cumin",
+  quantity= 300,
+  category = "Spices",
+  price:"$ 3.2/kg",
+  picture_url = "https://what what"
+},
+{
+  id =15,
+  product = "Coriander",
+  quantity= 300,
+  category = "Spices",
+  price:"$ 3.2/kg",
+  picture_url = "https://what what"
+},
+{
+  id =16,
+  product = "Turmeric",
+  quantity= 300,
+  category = "Spices",
+  price:"$ 3.2/kg",
+  picture_url = "https://what what"
+},
+
+
+
+{
+  id = 17,
+  product = "Bracelet",
+  quantity= 300,
+  category = "Craft",
+  price:"$ 2.0/box",
+  picture_url = "https://what what"
+},
+{
+  id =18,
+  product = "Macrame Earrings",
+  quantity= 300,
+  category = "Craft",
+  price:"$ 80.0/10 pack",
+  picture_url = "https://what what"
+},
+{
+  id = 19,
+  product = "Kutch embroidery",
+  quantity= 300,
+  category = "textile",
+  price:"$ 80.0/10 pack",
+  picture_url = "https://what what"
+},
+{
+  id = 20,
+  product = "Pattamadai mat",
+  quantity= 300,
+  category = "textile",
+  price:"$ 80.0/10 pack",
+  picture_url = "https://what what"
+},
+{
+  id = 21,
+  product = "jute bags",
+  quantity= 300,
+  category = "textile",
+  price:"$ 80.0/10 pack",
+  picture_url = "https://what what"
+},
 ]
